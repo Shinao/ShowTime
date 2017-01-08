b40cb7fc55b297376e6ea13f5c8b75a2bfce5073
Shinao <rmonnerat.pro@gmail.com>
2017-01-08 12:17:00 +0100

    Fix updating show when could not retrieve it

--- api/updateShows.php
+++ api/updateShows.php
@@ -12,6 +12,14 @@
 	foreach ($result as $row)
 	{
 		$search = $Client->TVMaze->getShow($row['api_id']);
+
+		// Check Show API_ID deleted from TVDB
+		if (strlen($search[0]->id) <= 0) {
+			echo "show deleted => " . $row['api_id'];
+			var_dump($search);
+			continue;
+		}
+
 		$show = $search[0];
 		$episodes = $search[1];
 		$last_episode = $episodes[count($episodes) - 1];
